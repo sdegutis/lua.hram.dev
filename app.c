04b67a84fd337dc362d1973b37cb4c2e1bb54440
Steven Degutis <sbdegutis@gmail.com>
2025-07-19 10:50:44 -0500

    EMBED!

--- app.c
+++ app.c
@@ -9,6 +9,10 @@
 #include "thread.h"
 #include "mutex.h"
 
+#include <Windows.h>
+#include <stdio.h>
+#include "resource.h"
+
 int luaopen_lpeg(lua_State* L);
 
 static lua_State* mvm;
@@ -63,104 +67,15 @@ void boot()
 
 	mvm = newvm();
 
-	printf("testing\n");
 
-	luaL_dostring(mvm,
-		"print(lpeg)\n"
-	);
+	HMODULE handle = GetModuleHandle(NULL);
+	HRSRC rc = FindResource(handle, MAKEINTRESOURCE(IDR_MYTEXTFILE), MAKEINTRESOURCE(TEXTFILE));
+	HGLOBAL rcData = LoadResource(handle, rc);
+	DWORD size = SizeofResource(handle, rc);
+	const char* data = LockResource(rcData);
 
-	//*
-	luaL_dostring(mvm,
-
-		"		-- foo\n"
-		"\n"
-		"		print(lpeg)\n"
-		"\n"
-		"		print(pcall(function()\n"
-		"\n"
-		"		print('in foo!')\n"
-		"\n"
-		"		--[=[\n"
-		"		m = mutex.create()\n"
-		"\n"
-		"		t = thread.create([[\n"
-		"			local m = ...\n"
-		"			mutex.lock(m)\n"
-		"			print('locked in thread')\n"
-		"			mutex.unlock(m)\n"
-		"			print('done in thread')\n"
-		"		]], m)\n"
-		"\n"
-		"		--thread.sleep(1000)\n"
-		"		mutex.lock(m)\n"
-		"		print('locked in main')\n"
-		"		mutex.unlock(m)\n"
-		"		print('done in main')\n"
-		"\n"
-		"\n"
-		"		-- print(t)\n"
-		"\n"
-		"		-- require 'bar'\n"
-		"\n"
-		"		--]=]\n"
-		"\n"
-		"\n"
-		"\n"
-		"		local m = memory.malloc(3*3*4)\n"
-		"		for i = 0,3*3*4-1 do memory.set(m+i, 8, math.random(0xff)-1) end\n"
-		"		local img = image.create(m, 3, 3)\n"
-		"		--memory.free(m)\n"
-		"\n"
-		"		image.copy(nil, img, 0, 0)\n"
-		"\n"
-		"		local m2 = memory.malloc(9*9*4)\n"
-		"		memory.fill(m2, 0, 9*9*4)\n"
-		"		local img2 = image.create(m2, 9, 9)\n"
-		"		memory.free(m2)\n"
-		"\n"
-		"		image.copy(img2, img, 0, 0, 0, 0, 3, 3)\n"
-		"		image.copy(img2, img, 6, 0, 0, 0, 3, 3)\n"
-		"		image.copy(img2, img, 0, 6, 0, 0, 3, 3)\n"
-		"		image.copy(img2, img, 6, 6, 0, 0, 3, 3)\n"
-		"\n"
-		"		image.delete(img)\n"
-		"\n"
-		"\n"
-		"\n"
-		"		function mousemove(x, y)\n"
-		"			--image.copy(nil, img2, x, y)\n"
-		"			image.update(nil, x, y, 2, 2, m+4*4, 3*4)\n"
-		"			print('mouse moved', x, y)\n"
-		"		end\n"
-		"\n"
-		"		function mousewheel(d)\n"
-		"			print('mouse wheel', d)\n"
-		"		end\n"
-		"\n"
-		"		function mouseup(b)\n"
-		"			print('mouse up', b)\n"
-		"		end\n"
-		"\n"
-		"		function mousedown(b)\n"
-		"			print('mouse up', b)\n"
-		"		end\n"
-		"\n"
-		"		function keyup(k)\n"
-		"			print('key up', k)\n"
-		"		end\n"
-		"\n"
-		"		function keydown(k)\n"
-		"			print('key up', k)\n"
-		"		end\n"
-		"\n"
-		"		function keychar(s)\n"
-		"			print('key char', s)\n"
-		"		end\n"
-		"\n"
-		"		end))\n"
-
-	);
-	//*/
+	luaL_loadbuffer(mvm, data, size, "boot");
+	lua_pcallk(mvm, 0, 0, 0, 0, NULL);
 }
 
 void mouseMoved(int x, int y) {
---
+++ boot.lua
@@ -0,0 +1,84 @@
+print(lpeg)
+
+print(pcall(function()
+
+print('in foo!')
+
+--[=[
+m = mutex.create()
+
+t = thread.create([[
+	local m = ...
+	mutex.lock(m)
+	print('locked in thread')
+	mutex.unlock(m)
+	print('done in thread')
+]], m)
+
+--thread.sleep(1000)
+mutex.lock(m)
+print('locked in main')
+mutex.unlock(m)
+print('done in main')
+
+
+-- print(t)
+
+-- require 'bar'
+
+--]=]
+
+
+
+local m = memory.malloc(3*3*4)
+for i = 0,3*3*4-1 do memory.set(m+i, 8, math.random(0xff)-1) end
+local img = image.create(m, 3, 3)
+--memory.free(m)
+
+image.copy(nil, img, 0, 0)
+
+local m2 = memory.malloc(9*9*4)
+memory.fill(m2, 0, 9*9*4)
+local img2 = image.create(m2, 9, 9)
+memory.free(m2)
+
+image.copy(img2, img, 0, 0, 0, 0, 3, 3)
+image.copy(img2, img, 6, 0, 0, 0, 3, 3)
+image.copy(img2, img, 0, 6, 0, 0, 3, 3)
+image.copy(img2, img, 6, 6, 0, 0, 3, 3)
+
+image.delete(img)
+
+
+
+function mousemove(x, y)
+	--image.copy(nil, img2, x, y)
+	image.update(nil, x, y, 2, 2, m+4*4, 3*4)
+	print('mouse moved', x, y)
+end
+
+function mousewheel(d)
+	print('mouse wheel', d)
+end
+
+function mouseup(b)
+	print('mouse up', b)
+end
+
+function mousedown(b)
+	print('mouse up', b)
+end
+
+function keyup(k)
+	print('key up', k)
+end
+
+function keydown(k)
+	print('key up', k)
+end
+
+function keychar(s)
+	print('key char', s)
+end
+
+end))
