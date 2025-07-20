36a5ff699cd6fae2eec11f381abb4977e843bc8d
Steven Degutis <sbdegutis@gmail.com>
2025-07-20 13:53:22 -0500

    FONTS!

--- boot.lua
+++ boot.lua
@@ -1,29 +1,7 @@
 local ok, err = pcall(function()
 
-
-local cs = sync.newcritsec()
-
-
-local t, err = sync.newthread([[
-	local a,b,c,cs = ...
-	sync.entercritsec(cs)
-	print('inside thread', table.pack(...).n, ...)
-	print('a='..tostring(a))
-	print('b='..tostring(b))
-	print('c='..tostring(c))
-	sync.leavecritsec(cs)
-	sync.delcritsec(cs)
-]], 123, nil, 'asdf', cs)
-
-sync.entercritsec(cs)
-sync.closehandle(t)
-print('outside thread')
-print(t, err)
-print('sleep')
-sync.sleep(1000)
-print('wake')
-sync.leavecritsec(cs)
-
+img = image.create(0x10100+(4*6*4*(97-32)), 4, 6)
+image.copy(nil, img, 1, 1)
 
 end)
 
