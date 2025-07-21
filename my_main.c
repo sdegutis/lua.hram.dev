3265ecf17dc28cc068efae751a389b4597db83ba
Steven Degutis <sbdegutis@gmail.com>
2025-07-21 14:37:47 -0500

    beta version

--- my_main.c
+++ my_main.c
@@ -4,6 +4,13 @@
 #include "my_app.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, PWSTR pCmdLine, int nCmdShow) {
+	SYSTEMTIME time;
+	GetSystemTime(&time);
+	if (time.wYear > 2025 || time.wMonth > 7 || time.wDay > 27) {
+		MessageBox(NULL, L"This HRAM beta version has expired, please get a new one, thanks!", L"HRAM beta version expired", 0);
+		return 0;
+	}
+
 	if (setupWindow(hInstance, nCmdShow)) return 1;
 	boot();
 	runLoop();
