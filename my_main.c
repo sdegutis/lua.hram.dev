6b1a5c2db47d8562503aa66a1af0dfc59e68828e
Steven Degutis <sbdegutis@gmail.com>
2025-08-14 22:17:35 -0500

    Remove beta blocker

--- my_main.c
+++ my_main.c
@@ -4,12 +4,12 @@
 #include "my_app.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, PWSTR pCmdLine, int nCmdShow) {
-	SYSTEMTIME time;
-	GetSystemTime(&time);
-	if (time.wYear > 2025 || time.wMonth > 7 || time.wDay > 27) {
-		MessageBox(NULL, L"This HRAM beta version has expired, please get a new one, thanks!", L"HRAM beta version expired", 0);
-		return 0;
-	}
+	//SYSTEMTIME time;
+	//GetSystemTime(&time);
+	//if (time.wYear > 2025 || time.wMonth > 7 || time.wDay > 27) {
+	//	MessageBox(NULL, L"This HRAM beta version has expired, please get a new one, thanks!", L"HRAM beta version expired", 0);
+	//	return 0;
+	//}
 
 	void* sysmem = VirtualAlloc(0x30000, 0x4000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	if (!sysmem) {
